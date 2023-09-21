838202556ab83824be99ff03e7112b89f9db04cb
BalajkaP <p.b.1@seznam.cz>
2023-09-21 19:23:20 +0200

    Uprava comment ve třídě CrApiTestClass

--- src/main/java/CriteriaApi/CrApiTestClass.java
+++ src/main/java/CriteriaApi/CrApiTestClass.java
@@ -16,14 +16,29 @@ public class CrApiTestClass {
 
     public CrApiTestClass(Session aSession){
         session = aSession;
-    }
+    }  // Hlavní třída si v konstruktoru převezme session
 
-    public void runItWithWhere(String name, Integer ageVetsi){
+    public void runItWithWhere(String name, Integer ageVetsi){  // Tato metoda vybírá data z DB
 
         // skladani vyberovych podminek pomoci objektu a nikoli HQL retezcu
 
+        // VYSVETLENÍ RŮZNÝCH VZTAHŮ MEZI API TŘÍDAMI A INTERFACE:
+        // FIND - co se používá v klasickém HIBERNATE, je METODA v INTERFACE EntityManager (ta je v package javax.persistence),
+        // ze kterého dědí naše INTERFACE Session
+        // getCriteriaBuilder - platí to samé, co pro FIND. Taky tato metoda je v INTERFACE EntityManager, ze kterého dědí
+        // naše Session interface
+    // ------------------------------------------------------------------------------------------
+
+        // Získání instance toho kde budeme tvořit kritéria. Metoda getCriteriaBuilder vrací objekt CriteriaBuilder.
+        // Na tomto objektu pak mohu tvořit QUERY.
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        // Zde vytvoříme dotaz criteriaQuery. Zde je důležité určit odkud (z jakého typu Entity) budeme brát data- zde z ActorEntity.
+        // metoda createQuery create a CriteriaQuery object with the specified result type - ActorEntity.class udává typ
+        // výsledného objektu. Takto definováno: <T> CriteriaQuery<T> createQuery(Class<T> resultClass), kde
+        // resultClass - je type of the query result.
+        // CriteriaQuery už obsahuje metody SELECT, WHERE,...
         CriteriaQuery<ActorEntity> criteriaQuery = criteriaBuilder.createQuery(ActorEntity.class);
+        // Nastavit root, protože ten SELECT může brát z více tabulek. Asi zde nastavím tu hlavní tab. Je to velmi komplexní.
         Root<ActorEntity> root = criteriaQuery.from(ActorEntity.class);
 
         List<Predicate> preList = new ArrayList<Predicate>(); // položky ve where
--- src/main/java/entityes/MovieDirectorEntity.java
+++ src/main/java/entityes/MovieDirectorEntity.java
@@ -3,22 +3,23 @@ package entityes;
 import javax.persistence.*;
 
 // TOTO UKÁZKA JAK NAMODELOVAT ENTITU V HIBERNATE KTERÁ SLOUČÍ HODNOTY ZE 2 TABULEK
+// Tj. vytvořím NOVOU FYZICKOU VÝSLEDKOVOU TABULKU,  a ne jen abstraktní TABULKU JAKO VÝSLEDEK query
 // JAK TEDY VYBÍRÁM ZE 2 TABULEK
 // entita reprezentuje přístup ke dvoum tabulkám - dodělat obsluhu.
 @Entity
-@Table(name="a_movie")
-@SecondaryTable(name="a_director")
+@Table(name="a_movie")             // Vybíráme z této tabulky a_movie
+@SecondaryTable(name="a_director") // ale taky z této tabulky  a_director
 public class MovieDirectorEntity {
 
     @Id
-    @Column(name="id_movie")
+    @Column(name="id_movie")        // 1. sloupec je movie ID
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="name")
+    @Column(name="name")            // 2. sloupec je movie NAME
     private String name;
 
-    @Column(name="name", table = "a_director")
+    @Column(name="name", table = "a_director")  // 3. sloupec bude director NAME - z tabulky a_director
     private String dirctorname;
 
 }
